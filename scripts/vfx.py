3d029c2d1b223425dc7e23a621084084486593a0
Marko Lialiuk <49954445+Robomarchello@users.noreply.github.com>
2021-07-29 11:25:26 +0300

    Create vfx.py

---
+++ scripts/vfx.py
@@ -0,0 +1,10 @@
+import pygame,math,random as rd
+
+class destroyUFO():
+    def __init__(self,pos):
+        self.pos = pos
+
+        self.angles = [45,135,225,315]
+
+    def draw(self,display):
+        pass
